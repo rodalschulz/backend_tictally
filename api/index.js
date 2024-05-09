9a3d5d5351e255be20b06b4f765ec0b0506dbf65
rodalschulz <rod.schulz.rosas@gmail.com>
2024-05-09 15:45:51 -0500

    remove CORS and user agent restrictions

--- api/index.js
+++ api/index.js
@@ -5,30 +5,31 @@ import v1router from "./routes/v1/v1rootRt.js";
 const app = express();
 const port = 3000;
 
-const allowedOrigins = ["https://frontend-dummy.vercel.app"];
-const corsOptions = {
-  origin: function (origin, callback) {
-    if (!origin || allowedOrigins.indexOf(origin) !== -1) {
-      callback(null, true);
-    } else {
-      callback(new Error("Not allowed by CORS"));
-    }
-  },
-};
+// const allowedOrigins = ["https://frontend-dummy.vercel.app"];
+// const corsOptions = {
+//   origin: function (origin, callback) {
+//     if (!origin || allowedOrigins.indexOf(origin) !== -1) {
+//       callback(null, true);
+//     } else {
+//       callback(new Error("Not allowed by CORS"));
+//     }
+//   },
+// };
 
 app.use(express.json());
-app.use(cors(corsOptions));
+// app.use(cors(corsOptions));
+app.use(cors());
 
-app.use((req, res, next) => {
-  const userAgent = req.get("User-Agent");
-  const allowedUserAgent =
-    "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
-  if (userAgent === allowedUserAgent) {
-    next();
-  } else {
-    res.status(403).send("Forbidden");
-  }
-});
+// app.use((req, res, next) => {
+//   const userAgent = req.get("User-Agent");
+//   const allowedUserAgent =
+//     "Mozilla/5.0 (Windows NT 10.0; Win64; x64) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/124.0.0.0 Safari/537.36";
+//   if (userAgent === allowedUserAgent) {
+//     next();
+//   } else {
+//     res.status(403).send("Forbidden");
+//   }
+// });
 
 app.get("/", (req, res) => {
   res.redirect("/v1");
