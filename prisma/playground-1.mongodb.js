50a89724c8e3fcfa5fc7c573c8423a4bece03b62
rodalschulz <rod.schulz.rosas@gmail.com>
2024-06-01 20:04:21 -0500

    modify mongodb validation and prisma schema

--- prisma/playground-1.mongodb.js
+++ prisma/playground-1.mongodb.js
@@ -3,6 +3,47 @@
 // db.users.createIndex({ username: 1 }, { unique: true });
 // db.users.createIndex({ email: 1 }, { unique: true });
 
+// db.runCommand({
+//   collMod: "users",
+//   validator: {
+//     $jsonSchema: {
+//       bsonType: "object",
+//       required: ["email", "username", "password", "verified"],
+//       properties: {
+//         email: {
+//           bsonType: "string",
+//           description: "must be a string and is required",
+//           pattern: "^\\S+@\\S+\\.\\S+$",
+//         },
+//         username: {
+//           bsonType: "string",
+//           description: "must be a string and is required",
+//           minLength: 3,
+//           maxLength: 22,
+//         },
+//         password: {
+//           bsonType: "string",
+//           description: "must be a string and is required",
+//         },
+//         globalRole: {
+//           enum: ["USER", "ADMIN", "MASTER"],
+//           description: "must be one of: USER, ADMIN, MASTER",
+//         },
+//         verified: {
+//           bsonType: "bool",
+//           description: "must be a boolean and is required",
+//         },
+//         verificationToken: {
+//           bsonType: "string",
+//           description: "must be a string if exists",
+//         },
+//       },
+//     },
+//   },
+//   validationLevel: "strict",
+//   validationAction: "error",
+// });
+
 // db.createCollection("users", {
 //   validator: {
 //     $jsonSchema: {
