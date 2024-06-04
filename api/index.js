47ebc7a4b35808183b0815bc6d25f6886ec3c779
rodalschulz <rod.schulz.rosas@gmail.com>
2024-06-03 22:51:06 -0500

    change api structure to avoid 'no more than 12 serverless funcs for hobby plan'

--- api/index.js
+++ api/index.js
@@ -1,5 +1,11 @@
 import express from "express";
-import v1router from "./routes/v1/v1rootRt.js";
+import multer from "multer";
+
+import v1authCtrl from "./controllers/v1authCtrl.js";
+import v1crudCtrl from "./controllers/v1crudCtrl.js";
+import v1queryCtrl from "./controllers/v1queryCtrl.js";
+import v1userConfigCtrl from "./controllers/v1userConfigCtrl.js";
+import authMw from "./middleware/authMw.js";
 import corsMw from "./middleware/v1corsMw.js";
 import apiKeyMw from "./middleware/v1apiKeyMw.js";
 
@@ -11,6 +17,45 @@ app.use(apiKeyMw.apiKeyCheck);
 app.get("/", (req, res) => {
   res.redirect("/v1");
 });
+
+const v1router = express.Router();
+
+v1router.get("/", async (req, res) => {
+  res.status(200).json({
+    response: "Welcome to the API. There is no data in this base v1 endpoint.",
+  });
+});
+
+v1router.post("/register", v1authCtrl.registerUser);
+v1router.post("/login", v1authCtrl.loginUser);
+v1router.get("/auth", v1authCtrl.isAuthenticated);
+v1router.get("/verify-email", v1authCtrl.verifyEmail);
+v1router.use(authMw.authCheck);
+
+v1router.get("/users/:userId/activity-data", v1crudCtrl.readActivities);
+v1router.post("/users/:userId/activity-data", v1crudCtrl.createActivity);
+v1router.patch("/users/:userId/activity-data", v1crudCtrl.updateActivity);
+v1router.delete("/users/:userId/activity-data", v1crudCtrl.deleteActivity);
+
+const upload = multer({ storage: multer.memoryStorage() });
+v1router.post(
+  "/users/:userId/upload-csv",
+  upload.single("file"),
+  v1crudCtrl.uploadActivities
+);
+v1router.get("/users/:userId/download-activity", v1crudCtrl.downloadActivities);
+
+v1router.get("/users/:userId/query-activity-data", v1queryCtrl.queryActivities);
+
+v1router.get(
+  "/users/:userId/category-config",
+  v1userConfigCtrl.readCategConfig
+);
+v1router.patch(
+  "/users/:userId/category-config",
+  v1userConfigCtrl.updateCategConfig
+);
+
 app.use("/v1", v1router);
 
 const PORT = process.env.PORT || 3001;
